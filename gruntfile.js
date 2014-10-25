3446dfd4bfb91bd574cadd3f481f13463791947c
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2014-10-25 17:43:00 +0200

    grunt

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            default: {
+            }
+        },
+
+        cssmin: {
+            default: {
+            }
+        },
+	shell: {
+            options: {
+                stderr: false
+            },
+            target: {
+                command: 'bower --allow-root install &&  mkdir -p web/js2 && rm -rf web/js2/* && mv -f bower_components/** web/js2/ && rm -rf bower_components && cd web/js2/bootstrap/ && npm install && grunt --force --no-bin-links'
+            }
+        }
+    });
+
+    // Ici on indique quel plugin on utilise
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.registerTask('default', ['shell','uglify', 'cssmin']);
+};
