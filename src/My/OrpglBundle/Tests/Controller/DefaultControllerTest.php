824133cd2f2a8cd6857fbedd56170b7fb2530d04
vagrant <vagrant@precise64.(none)>
2014-11-25 11:01:28 +0000

    test suite fix2

--- src/My/OrpglBundle/Tests/Controller/DefaultControllerTest.php
+++ src/My/OrpglBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,7 +9,7 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
 
-	$ch = curl_init("http://127.0.0.1/app_dev.php/login_check");
+	$ch = curl_init("http://127.0.0.1:80/app_dev.php/login_check");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$result = curl_exec($ch);
 	$tr= (stristr($result,"SERVER IS DOWN")!==false)?true:false;
